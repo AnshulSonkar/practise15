9c054b776a84d405b207d9fdc59d85a3c2531b2b
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-03-14 23:29:19 +0530

    initial

---
+++ json.js
@@ -0,0 +1,20 @@
+let input = document.getElementById("nameInput");
+let button = document.getElementById("saveBtn");
+let output = document.getElementById("output");
+
+button.addEventListener("click", function () {
+
+  let user = {
+    name: input.value
+  };
+
+  localStorage.setItem("user", JSON.stringify(user));
+
+  output.textContent = "Saved!";
+});
+
+let savedUser = JSON.parse(localStorage.getItem("user"));
+
+if (savedUser) {
+  output.textContent = "Stored Name: " + savedUser.name;
+}
